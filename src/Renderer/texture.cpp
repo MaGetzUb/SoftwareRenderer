25017c3a3d5ea30b16ef014d99430a4b7b1378d5
MaGetzUb <marko.ranta95@gmail.com>
2018-07-08 06:48:46 +0300

    Added texture class, not tested yet.

---
+++ src/Renderer/texture.cpp
@@ -0,0 +1,91 @@
+/*
+Copyright © 2018, Marko Ranta
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright
+	   notice, this list of conditions and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the above copyright
+	   notice, this list of conditions and the following disclaimer in the
+	   documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "texture.hpp"
+#include "../stb/stb_image.h" 
+
+#define STB_IMAGE_IMPLEMENTATION 
+
+
+Texture::Texture(const std::string& path) {
+	load(path);
+}
+
+bool Texture::load(const std::string& path) {
+	
+	int channels; 
+	stbi_uc* pixels = stbi_load(path.c_str(), &mSize.x, &mSize.y, &channels, 4);
+	if(!pixels)
+		return false; 
+	mPixels = new vec4[mSize.x * mSize.y];
+	for(int y = 0; y < mSize.y; y++)
+	for(int x = 0; x < mSize.x; x++)
+	{
+		mPixels[x + y * mSize.x] = ToVec4(*(int*)pixels);
+		pixels += 4;
+	}
+	stbi_image_free(pixels);
+	return true;
+}
+
+vec4 Texture::sample(float x, float y, Sampling sampling, Wraping wraping) const {
+
+	switch(wraping) {
+		case Wraping::Repeat:
+			x = QMod(x * mSize.x, (float)mSize.x);
+			y = QMod(y * mSize.y, (float)mSize.y);
+		break;
+		case Wraping::Clamp:
+			x = Clamp(x * mSize.x, 0.f, (float)mSize.x);
+			y = Clamp(y * mSize.y, 0.f, (float)mSize.y);
+		break;
+	}
+	switch(sampling) {
+		case Sampling::None: return sample((int)x, (int)y); break;
+		case Sampling::Linear: 
+		case Sampling::CubicHermite:
+		{
+			float fracX = x - (int)x;
+			float fracY = y - (int)y; 
+
+			if(sampling == Sampling::CubicHermite) {
+				fracX = fracX * fracX * (3.0 - 2.0*fracX);
+				fracY = fracY * fracY * (3.0 - 2.0*fracX);
+			}
+
+			vec4 a = sample((int)x, (int)y);
+			vec4 b = sample((int)x + 1, (int)y);
+			vec4 c = sample((int)x, (int)y + 1);
+			vec4 d = sample((int)x + 1, (int)y + 1);
+			
+			return a + fracX * (b - a) + fracY * (c - a) * (1.0f - fracX) + fracX * fracY * (d - b);
+		}
+	
+	}
+
+	return vec4{ 0.0f };
+
+}
---
+++ src/Renderer/texture.hpp
@@ -0,0 +1,95 @@
+/*
+Copyright © 2018, Marko Ranta
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+	1. Redistributions of source code must retain the above copyright
+	   notice, this list of conditions and the following disclaimer.
+
+	2. Redistributions in binary form must reproduce the above copyright
+	   notice, this list of conditions and the following disclaimer in the
+	   documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDER BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef TEXTURE_HPP
+#define TEXTURE_HPP 
+
+#include "../Math/vec4.hpp"
+#include "../Math/vec2.hpp"
+#include <string>
+
+class Texture { 
+	
+	vec4* mPixels = nullptr; //Store the pixel data as floats, so there's no need for byte -> float conversion.
+	ivec2 mSize; 
+	
+	public:
+		
+		enum class Sampling {
+			None,
+			Linear,
+			CubicHermite
+		};
+
+		enum class Wraping {
+			Clamp,
+			Repeat
+		};
+
+	
+		Texture() = default; 
+		
+		Texture(const Texture& b) = delete; 
+		
+		Texture(Texture&& b) {
+			std::swap(mPixels, b.mPixels);
+			std::swap(mSize, b.mSize);
+		}
+
+		Texture(const std::string& path);
+
+		~Texture() { delete [] mPixels; }
+		
+		Texture& operator=(Texture&& b) {
+			std::swap(mPixels, b.mPixels);
+			std::swap(mSize, b.mSize);
+			return *this;
+		}
+
+
+		bool load(const std::string& path);
+		
+		int width() const { return mSize.x; }
+
+		int height() const { return mSize.y; }
+
+		inline vec4 sample(int x, int y) const { 
+			assert(x >= 0); 
+			assert(y >= 0); 
+			assert(x < mSize.x); 
+			assert(y < mSize.x); 
+			return mPixels[x + y * mSize.x]; 
+		}
+
+		vec4 sample(float x, float y, Sampling sampling = Sampling::None, Wraping wraping = Wraping::Repeat) const;
+
+		inline vec4 sample(const vec2& vec, Sampling sampling = Sampling::None, Wraping wraping = Wraping::Repeat) const { return sample(vec.x, vec.y, sampling, wraping); }
+};
+
+
+#endif //TEXTURE_HPP
+
+
